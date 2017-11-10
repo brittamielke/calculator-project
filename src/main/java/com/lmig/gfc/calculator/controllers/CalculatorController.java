0bf9eb4a7c3ce40d46b21252e610bf76d01cf138
Britta Mielke <britta.mielke@libertymutual.com>
2017-11-10 10:48:34 -0500

    Initial upload of code- Complete & Satisfactory

---
+++ src/main/java/com/lmig/gfc/calculator/controllers/CalculatorController.java
@@ -0,0 +1,48 @@
+package com.lmig.gfc.calculator.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.lmig.gfc.calculator.models.Calculator;
+
+@Controller
+public class CalculatorController {
+
+	private Calculator calc;
+	private double result;
+	
+	public CalculatorController() {
+		calc = new Calculator();
+	}
+
+	@RequestMapping("/")
+	public ModelAndView showApp(String firstNumber, String secondNumber, String operator) {
+		
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("calculator");
+		if (firstNumber !=null && firstNumber.length() > 0) {
+//use this to check the console to see if it ever entered the if statement
+			System.out.println("Im in the if");
+			if (operator.equals("+")) {
+				result = calc.add(firstNumber, secondNumber);
+			}
+			if (operator.equals("-")) {
+				result = calc.subtract(firstNumber, secondNumber);
+			}
+			if (operator.equals("*")) {
+				result = calc.multiply(firstNumber, secondNumber);
+			}
+			if (operator.equals("/")) {
+				result = calc.divide(firstNumber, secondNumber);
+			}
+			if (operator.equals("^")) {
+				result = calc.divide(firstNumber, secondNumber);
+			}
+		}
+		System.out.println(result);
+		mv.addObject("result", result);
+		return mv;
+
+	}
+}
---
+++ src/main/java/com/lmig/gfc/calculator/models/Calculator.java
@@ -0,0 +1,29 @@
+package com.lmig.gfc.calculator.models;
+
+public class Calculator {
+
+//public Calculator() {
+//	resultsList = new ArrayList<Result>()
+//}
+
+	public double add(String firstNumber, String secondNumber) {
+		double result = Double.parseDouble(firstNumber) + Double.parseDouble(secondNumber);
+		return result;
+	}
+	public double subtract(String firstNumber, String secondNumber) {
+		double result = Double.parseDouble(firstNumber) + Double.parseDouble(secondNumber);
+		return result;
+	}
+	public double multiply(String firstNumber, String secondNumber) {
+		double result = Double.parseDouble(firstNumber) + Double.parseDouble(secondNumber);
+		return result;
+	}
+	public double divide(String firstNumber, String secondNumber) {
+		double result = Double.parseDouble(firstNumber) + Double.parseDouble(secondNumber);
+		return result;
+	}
+	public double exponent(String firstNumber, String secondNumber) {
+		double result = Double.parseDouble(firstNumber) + Double.parseDouble(secondNumber);
+		return result;
+	}
+}
